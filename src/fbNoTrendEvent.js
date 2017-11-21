7843ba04aa447af90d2b7bf7a40aefd9ead6fd94
FindingNamo <davidminhnamngo@gmail.com>
2017-11-21 12:58:11 -0800

    Update fbNoTrendEvent.js

--- src/fbNoTrendEvent.js
+++ src/fbNoTrendEvent.js
@@ -1,7 +1,5 @@
  chrome.webNavigation.onCompleted.addListener(function(e) {
 	 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {event: "onCompleted"}, function(response) {
-		});
+		chrome.tabs.sendMessage(tabs[0].id, {event: "onCompleted"});
 	});
-
 }, {url: [{hostSuffix: 'facebook.com'}]});
