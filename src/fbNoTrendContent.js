7186fd59750171a32c98f7778551170c8b291173
FindingNamo <davidminhnamngo@gmail.com>
2017-11-21 12:52:55 -0800

    Update fbNoTrendContent.js

--- src/fbNoTrendContent.js
+++ src/fbNoTrendContent.js
@@ -1,1 +1,15 @@
+// Id of the div containing the trending topics, gateway to the cancerous comments. Yuck.
+const fbTrendingDivId = "pagelet_trending_tags_and_topics";
 
+// Remove an element if found
+function removeElementById(id) {
+    let element = document.getElementById(id);
+    if(element){
+      element.remove();
+    }
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+   removeElementById(fbTrendingDivId);
+  });
