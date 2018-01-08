cac1178dc92ca40c51a7f76bb4c592c597b32988
Josh Goldberg <joshuakgoldberg@outlook.com>
2018-01-08 00:58:54 -0800

    Added a CSS style to hide the trending div
    It was still showing up for me a little bit then getting removed. Very annoying. This visually removes it before removing it from the page.

--- src/fbNoTrendContent.js
+++ src/fbNoTrendContent.js
@@ -1,13 +1,25 @@
 // Id of the div containing the trending topics, gateway to the cancerous comments. Yuck.
 const fbTrendingDivId = "pagelet_trending_tags_and_topics";
 
+// Creates a CSS style to hide an element by id
+function hideElementById(id) {
+    const style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = "#pagelet_trending_tags_and_topics { display: none !important }";
+    document.getElementsByTagName("head")[0].appendChild(style);
+}
+
 // Remove an element if found
 function removeElementById(id) {
-    let element = document.getElementById(id);
+    const element = document.getElementById(id);
     if(element){
       element.remove();
     }
 }
+
+setTimeout(() => {
+    hideElementById(fbTrendingDivId);
+});
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
