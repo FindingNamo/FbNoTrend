aa1e23f81284bd2253320dfb53b505acbd66eae2
FindingNamo <davidminhnamngo@gmail.com>
2018-01-08 21:41:55 -0800

    Removing addition of CSS at runtime in favor of additional CSS content file

--- src/fbNoTrendContent.js
+++ src/fbNoTrendContent.js
@@ -1,14 +1,6 @@
 // Id of the div containing the trending topics, gateway to the cancerous comments. Yuck.
 const fbTrendingDivId = "pagelet_trending_tags_and_topics";
 
-// Creates a CSS style to hide an element by id
-function hideElementById(id) {
-    const style = document.createElement("style");
-    style.type = "text/css";
-    style.innerHTML = `#${id} { display: none !important }`;
-    document.getElementsByTagName("head")[0].appendChild(style);
-}
-
 // Remove an element if found
 function removeElementById(id) {
     const element = document.getElementById(id);
@@ -16,10 +8,6 @@ function removeElementById(id) {
       element.remove();
     }
 }
-
-setTimeout(() => {
-    hideElementById(fbTrendingDivId);
-});
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
